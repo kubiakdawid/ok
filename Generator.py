5c554d34808b31c45a61aa9982c1cfff227478a3
Jul14nn4 <julianna.zurczak03@gmail.com>
2025-03-19 14:52:24 +0100

    1.2 Program wczytujące dane z pliku (zostawiam gui dla testów)

--- Generator.py
+++ Generator.py
@@ -1,4 +1,3 @@
-import random
 import itertools
 import matplotlib
 import networkx as nx
@@ -6,11 +5,13 @@ import matplotlib.pyplot as plt
 
 matplotlib.use('TkAgg')
 
+
 def is_valid_coloring(graph, coloring):
     for u, v in graph.edges():
         if coloring[u] == coloring[v]:
             return False
     return True
+
 
 def greedy_coloring(graph):
     coloring = {}
@@ -19,23 +20,32 @@ def greedy_coloring(graph):
         coloring[node] = next(color for color in itertools.count() if color not in adjacent_colors)
     return coloring
 
-n_nodes = 5 #Ilość wierzchołków
 
-G = nx.Graph()
-G.add_nodes_from(range(n_nodes))
+def load_graph_from_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
 
-for i in range(n_nodes):
-    for j in range(i + 1, n_nodes):
-        if random.random() < 0.5: #Gęstość grafu
-            G.add_edge(i, j)
+        n_nodes = int(lines[0].strip())
+
+        G = nx.Graph()
+        G.add_nodes_from(range(n_nodes))
+
+        for line in lines[1:]:
+            u, v = map(int, line.strip().split())
+            G.add_edge(u, v)
+
+        return G
+
+
+filename = "graph.txt"  #Nazwa pliku
+G = load_graph_from_file(filename)
 
 coloring_result = greedy_coloring(G)
 
-print('Coloring:', coloring_result)
-print('Valid:', is_valid_coloring(G, coloring_result))
-print('K:', len(set(coloring_result.values())))
+print('Coloring:', coloring_result) #Kolory wierzchołków
+print('Valid:', is_valid_coloring(G, coloring_result)) #Poprawność
+print('K:', len(set(coloring_result.values()))) #Ilość kolorów
 
 color_map = [coloring_result[node] for node in G.nodes()]
 nx.draw(G, node_color=color_map, with_labels=True, font_weight='bold')
-
 plt.show()
