876fefdf7b092030eb9142265e05b422e7e5b9f2
kubiakdawid <agnieszka.lis.28@interia.pl>
2025-03-21 16:08:03 +0100

    v0.1.2: Flask&Render - fix

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
